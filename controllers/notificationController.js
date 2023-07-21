b550b0d3dfdc2a9aa165178b08034de0d2bb315d
parthv2023 <parth@creativegeeks.online>
2023-07-21 11:45:02 +0530

    send mail functionality

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,6 +1,7 @@
-const cron = require('node-cron');
+const nodemailer = require('nodemailer');
 const Event = require('../models/eventSchema');
 const User = require('../models/userSchema');
+const cron = require('node-cron')
 
 module.exports = {
     scheduleEventNotification: async (eventId, userId) => {
@@ -12,9 +13,32 @@ module.exports = {
         }
 
         const notificationTime = new Date(event.datetime.getTime() - 10 * 60000);
+        
+        const task = cron.schedule(notificationTime.getMinutes() + ' ' + notificationTime.getHours() + ' ' + notificationTime.getDate() + ' ' + (notificationTime.getMonth() + 1) + ' *', async () => {
 
-        const task = cron.schedule(notificationTime.getMinutes() + ' ' + notificationTime.getHours() + ' ' + notificationTime.getDate() + ' ' + (notificationTime.getMonth() + 1) + ' *', () => {
-            console.log(`Sending notification to user ${user.username} for event ${event.title}`);
+            try {
+                const transporter = nodemailer.createTransport({
+                    host: 'smtp.gmail.com',
+                    port: 587,
+                    secure: false,
+                    auth: {
+                        user: 'ritiksoni.dignizant@gmail.com',
+                        pass: 'vodrnbazxdosrrea',
+                    },
+                });
+
+                await transporter.sendMail({
+                    from: 'ritiksoni.dignizant@gmail.com',
+                    to: user.username,
+                    subject: 'Event Notification',
+                    text: `Hello, you have an upcoming event "${event.title}" scheduled at ${event.datetime} in ${event.location}.`,
+                });
+
+                console.log('Email sent successfully.');
+            } catch (error) {
+                console.error('Error sending email:', error);
+            }
+
             task.stop();
         });
 
